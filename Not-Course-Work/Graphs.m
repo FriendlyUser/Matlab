130b0063947b547ed1b60b48025b7b08fb445222
David Li <FriendlyUser@users.noreply.github.com>
2016-06-13 19:22:55 -0700

    Add files via upload

---
+++ Not-Course-Work/Graphs.m
@@ -0,0 +1,8 @@
+s = [ 3 2 4 2 8 7 7 6 5 6 7 1 1];
+t = [ 4 5 8 3 7 4 3 3 6 2 6 2 5];
+weights = [ 6 2 7 5 3 4 5 6 4 5 1 6 1];
+names = {'A' 'B' 'C' 'D' 'E' 'F' 'G' 'H'};
+G = graph(s,t,weights,names);
+p = plot(G,'EdgeLabel',G.Edges.Weight);
+[T,pred] = minspantree(G);
+highlight(p,T)
