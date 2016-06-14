68c7519033c40d28179e17c6b87af0b991249b5d
David Li <FriendlyUser@users.noreply.github.com>
2016-06-14 10:44:20 -0700

    Update Graphs.m

--- Not-Course-Work/Graphs.m
+++ Not-Course-Work/Graphs.m
@@ -1,6 +1,6 @@
-s = [ 3 2 4 2 8 7 7 6 5 6 7 1 1];
-t = [ 4 5 8 3 7 4 3 3 6 2 6 2 5];
-weights = [ 6 2 7 5 3 4 5 6 4 5 1 6 1];
+s =       [ 3 2 4 2 8 7 7 6 5 6 7 1 1];
+t =       [ 4 5 8 3 7 4 3 3 6 2 6 2 5];
+weights = [ 6 2 7 5 3 5 4 5 1 2 3 6 1];
 names = {'A' 'B' 'C' 'D' 'E' 'F' 'G' 'H'};
 G = graph(s,t,weights,names);
 p = plot(G,'EdgeLabel',G.Edges.Weight);
