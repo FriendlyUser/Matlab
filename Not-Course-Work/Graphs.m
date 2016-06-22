1e1d978510d1899583ba084b3543ed4f4e843034
David Li <FriendlyUser@users.noreply.github.com>
2016-06-21 19:53:34 -0700

    Update Graphs.m

--- Not-Course-Work/Graphs.m
+++ Not-Course-Work/Graphs.m
@@ -1,3 +1,6 @@
+% David Li
+% Graphs.m
+% This program will plot and highlight a minimum spanning tree
 s =       [ 3 2 4 2 8 7 7 6 5 6 7 1 1];
 t =       [ 4 5 8 3 7 4 3 3 6 2 6 2 5];
 weights = [ 6 2 7 5 3 5 4 5 1 2 3 6 1];
